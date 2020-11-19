8f16ed832571f84f90213893ff75fb0473dbbf24
Russell <russelldev@protonmail.com>
2020-11-19 20:09:30 +0000

    add switch

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const itemList = [];
 
 
 while (addItem !== 'q'){
-   addItem = prompt('Please select action:');
-   switch (addItem) {
-       case addItem = 'n':
-           console.log(addItem);
-           break;
-           case addItem = 'l':
-               console.log('l');
-               break;
-               case addItem = 'q':
-                   addItem = 'q';
-   }
-
-
+    switch (addItem) {
+        case addItem = 'n':
+            console.log(addItem);
+            break;
+            case addItem = 'l':
+                console.log('l');
+                break;
+                case addItem = 'q':
+                    addItem = 'q';
+                }
+                
+                
+addItem = prompt('Please select action:');
     }
 console.log('Quitting')
