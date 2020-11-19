109df55489fc237b71ae497e8721e820a07f6efa
Russell <russelldev@protonmail.com>
2020-11-19 19:54:33 +0000

    declaring vars

---
+++ app.js
@@ -0,0 +1,10 @@
+let addItem = prompt('Please select action:');
+console.log(addItem);
+const itemList = [];
+
+
+while (addItem !== 'q'){
+   addItem = prompt('Please select action:');
+   console.log(addItem);
+    }
+console.log('Quittingg')
