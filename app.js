d78896079ff860d0ad383e8fe91cf80c0b59bb6e
Russell <russelldev@protonmail.com>
2020-11-19 20:12:46 +0000

    add full switch

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ while (addItem !== 'q'){
         case addItem = 'n':
             console.log(addItem);
             break;
-            case addItem = 'l':
-                console.log('l');
-                break;
-                case addItem = 'q':
-                    addItem = 'q';
-                }
-                
-                
+        case addItem = 'l':
+            console.log('l');
+            break;
+        case addItem = 'd':
+            console.log(addItem);
+            break;
+        case addItem = 'q':
+            addItem = 'q';
+            break;
+        }
 addItem = prompt('Please select action:');
     }
 console.log('Quitting')
